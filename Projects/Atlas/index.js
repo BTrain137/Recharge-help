3b8a6e40a650ab9a21346c8abb1762384e0b581a
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-03-22 21:42:56 -0700

    Laying out everything to create subscription

--- Projects/Atlas/index.js
+++ Projects/Atlas/index.js
@@ -4,8 +4,6 @@ import Recharge from "recharge-api-node";
 import Shopify from "shopify-api-node";
 import neatCsv from "neat-csv";
 
-import { deleteOneSubscription } from "./controller.js";
-
 dotenv.config();
 
 const {
@@ -29,25 +27,63 @@ const shopify = new Shopify({
 
 const fileLocation = "./data/1beancounter_subscriptions_update.csv";
 
+const sleep = (timeInSeconds = 500) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, timeInSeconds);
+  });
+};
+
 const main = async () => {
   const data = await readFile(new URL(fileLocation, import.meta.url));
   const csvArr = await neatCsv(data);
   let startNum = 0;
-  let endNum = 2;
+  let endNum = 1;
 
   for (let i = startNum; i < endNum; i++) {
     // console.log(`Row #${i}`, csvArr[i]);
+
     const row = csvArr[i];
-    const { subscription_id } = row;
+    const { subscription_id, properties, customer_email } = row;
     const newProductId = +row["NEW Product ID"];
     const newQty = +row["NEW Quantity"];
-    const newPrice = +row["NEW price"].replace("$", "");;
+    const newPrice = +row["NEW price"].replace("$", "");
 
+    // console.log({ newProductId, newQty, newPrice, subscription_id });
+    // console.log(properties);
 
-    console.log({ newProductId, newQty, newPrice, subscription_id });
+    try {
+      const product = await shopify.product.get(newProductId);
+      const { variants } = product;
+      const variantId = variants[0].id;
 
-    const result = await shopify.product.get(newProductId);
-		console.log(result);
+      // console.log(variantId);
+
+      const subscription = await recharge.subscription.get(subscription_id);
+      const {
+				address_id,
+        charge_interval_frequency,
+				next_charge_scheduled_at,
+        order_interval_frequency,
+        order_interval_unit,
+      } = subscription;
+
+      // console.log({
+      //   charge_interval_frequency,
+      //   order_interval_frequency,
+      //   order_interval_unit,
+			// 	next_charge_scheduled_at,
+			// 	address_id,
+      // });
+
+      sleep();
+    } catch (error) {
+			console.log(`===============================`);
+			console.log(`Row #${i}`, customer_email);
+			console.log(error);
+			console.log(`===============================`);
+		}
   }
 };
 

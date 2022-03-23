1efbe45a02dd8c12bf68b3e1f59c879f95f56687
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-03-22 21:08:11 -0700

    Create csv file

--- Projects/Atlas/index.js
+++ Projects/Atlas/index.js
@@ -1,5 +1,8 @@
+import { readFile } from "fs/promises";
 import dotenv from "dotenv";
 import Recharge from "recharge-api-node";
+import neatCsv from "neat-csv";
+
 import { deleteOneSubscription } from "./controller.js";
 
 dotenv.config();
@@ -11,9 +14,14 @@ const recharge = new Recharge({
   secrete: RECHARGE_API_SECRETE,
 });
 
+const fileLocation = "./data/1beancounter_subscriptions_update.csv";
+
 const main = async () => {
-  const result = await deleteOneSubscription(00000000, recharge);
-  console.log(result);
+  const data = await readFile(new URL(fileLocation, import.meta.url));
+  const csvArr = await neatCsv(data);
+  for (let i = 0; i < csvArr.length; i++) {
+    console.log(`Row #${i}`, csvArr[i]);
+  }
 };
 
 main();

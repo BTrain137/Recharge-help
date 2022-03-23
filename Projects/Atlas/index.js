658f72f93c6d760b4515ff8bcd41c5978eafd5e7
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-03-22 21:22:16 -0700

    Get product detail from shopify

--- Projects/Atlas/index.js
+++ Projects/Atlas/index.js
@@ -1,17 +1,30 @@
 import { readFile } from "fs/promises";
 import dotenv from "dotenv";
 import Recharge from "recharge-api-node";
+import Shopify from "shopify-api-node";
 import neatCsv from "neat-csv";
 
 import { deleteOneSubscription } from "./controller.js";
 
 dotenv.config();
 
-const { RECHARGE_API_KEY, RECHARGE_API_SECRETE } = process.env;
+const {
+  RECHARGE_API_KEY,
+  RECHARGE_API_SECRETE,
+  SHOPIFY_SHOP,
+  SHOPIFY_API_KEY,
+  SHOPIFY_PASSWORD,
+} = process.env;
 
 const recharge = new Recharge({
   apiKey: RECHARGE_API_KEY,
   secrete: RECHARGE_API_SECRETE,
+});
+
+const shopify = new Shopify({
+  shopName: SHOPIFY_SHOP,
+  apiKey: SHOPIFY_API_KEY,
+  password: SHOPIFY_PASSWORD,
 });
 
 const fileLocation = "./data/1beancounter_subscriptions_update.csv";
@@ -19,8 +32,22 @@ const fileLocation = "./data/1beancounter_subscriptions_update.csv";
 const main = async () => {
   const data = await readFile(new URL(fileLocation, import.meta.url));
   const csvArr = await neatCsv(data);
-  for (let i = 0; i < csvArr.length; i++) {
-    console.log(`Row #${i}`, csvArr[i]);
+  let startNum = 0;
+  let endNum = 2;
+
+  for (let i = startNum; i < endNum; i++) {
+    // console.log(`Row #${i}`, csvArr[i]);
+    const row = csvArr[i];
+    const { subscription_id } = row;
+    const newProductId = +row["NEW Product ID"];
+    const newQty = +row["NEW Quantity"];
+    const newPrice = +row["NEW price"].replace("$", "");;
+
+
+    console.log({ newProductId, newQty, newPrice, subscription_id });
+
+    const result = await shopify.product.get(newProductId);
+		console.log(result);
   }
 };
 

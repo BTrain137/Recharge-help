703b14b6ce27aa83f7e872b1d9e1be8027b09655
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2022-03-22 21:42:36 -0700

    No need for controller

--- Projects/Atlas/controller.js
+++
@@ -1,9 +0,0 @@
-export const deleteOneSubscription = (subscriptionId, recharge) => {
-  return new Promise((resolve, reject) => {
-    recharge.subscription
-      .delete(subscriptionId)
-      .then((result) => resolve(result))
-      .catch((err) => reject(err));
-  });
-};
-
